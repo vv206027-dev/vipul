02f43e514ffdaa965c870e5394aeab50182330e2
vv206027-dev <vv206027@gmail.com>
2026-01-28 16:46:11 +0530

    Add files via upload
    THIS IS PYTHON PROJECT

---
+++ Addition and string.py
@@ -0,0 +1,24 @@
+#Write a program of addition multiplication and subtraction and division:
+a=int(input("Enter your first No :"))
+b=int(input("Enter your second No :"))
+c=a+b
+print("Addition is " ,c)
+c=a-b
+print("Subtraction is " ,c)
+c=a*b
+print("Multiplication is " ,c)
+c=a/b
+print("Division is " ,c)
+c=a//b
+print("divsion is " ,c)
+c=a%b
+print("Percentage is " ,c)
+
+#Write a program of first name and last name in a single a full name:
+first_name=input("Enter a first name:")
+last_name=input("Enter a last name:")
+#concatenate first and last name with a space in between form the full name:
+full_name=first_name+""+last_name
+#print a personalized greeting  messege the using the full name
+print("hello,"+full_name + "!  welcome to the vipul.")
+
---
+++ aw.py
@@ -0,0 +1,15 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=========== RESTART: C:/Users/vv206/Downloads/Addition and string.py ===========
+Enter your first No :45
+Enter your second No :12
+Addition is  57
+Subtraction is  33
+Multiplication is  540
+Division is  3.75
+divsion is  3
+Percentage is  9
+Enter a first name:vipulvarshney
+Enter a last name:varshney
+hello,vipulvarshneyvarshney!  welcome to the vipul.
