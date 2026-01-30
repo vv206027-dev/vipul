b50bf1ad1eb70b16d3765e4694626b46120de457
vv206027-dev <vv206027@gmail.com>
2026-01-30 21:26:54 +0530

    even or odd method if else

---
+++ even or odd task1.py
@@ -0,0 +1,8 @@
+ #Write a program of even no a and  check even or odd:
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+
+
---
+++ task 2 for loop statement and total no sum.py
@@ -0,0 +1,5 @@
+#Uses a for loop to iterate over numbers from 1 to 50.
+number = int(input("Enter the number: "))
+
+for i in range(1, number+1):
+    print(sum(i))
